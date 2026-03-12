d87e99371ce3e4d27082007e39507ec3129a6b7e
TorunW <torun.wikstrom@gmail.com>
2026-03-12 15:03:37 +0100

    change page to link

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import Link from 'next/link';
 import Grid from '../components/grid';
 import Player from '../components/player';
 
 export default function Home() {
   return (
     <div className='h-screen w-screen items-center justify-center flex flex-col gap-20 bg-slate-950'>
-      <Link
-        href={'/testing'}
+      <a
+        href='https://www.notion.so/TicTacToe-Testing-Playground-2b7b720eca55802f96cbed0fb31ad719?source=copy_link'
+        target='_blank'
+        rel='noopener noreferrer'
         className='bg-slate-600 text-sm md:text-base hover:bg-slate-700 hover:cursor-pointer text-slate-200 font-bold py-2 px-4 rounded inline-flex items-center'
       >
         Click here to see testing
-      </Link>
+      </a>
       <Player />
       <Grid />
     </div>
