62942c4dbd9458573cb0446461a18ef7ca97e35f
TorunW <torun.wikstrom@gmail.com>
2026-03-17 10:57:29 +0100

    added playwright

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,42 +1,30 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Equivalence partitioning', () => {
-  test('has title', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+//todo: a fixture that is a get cell by number
 
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
-  });
+test('Verify that valid moves (empty cells) are accepted', async ({ page }) => {
+  await page.goto('https://tic-tac-toe-tau-lyart-50.vercel.app/');
+  const cellOne = page.getByRole('button', { name: 'Cell 1' });
 
-  test(
-    'Verify that valid moves (empty cells) are accepted and invalid moves (occupied cells) are rejected using equivalence partitions.',
-    {
-      annotation: [{ type: 'ticket-id', description: 'TC00-1' }],
-    },
-    async ({ page }) => {
-      await page.goto('https://playwright.dev/');
+  // Click the 1st cell in the grid.
+  await cellOne.click();
+  await page.getByRole('button', { name: 'Cell 2' }).click();
+  await page.getByRole('button', { name: 'Cell 3' }).click();
+  //await page.getByRole('image', { name: 'X' })
 
-      // Click the get started link.
-      await page.getByRole('link', { name: 'Get started' }).click();
+  // Expect a image called "X"
+  await expect(cellOne.getByAltText('X')).toBeVisible();
+});
 
-      const installationHeading = page.getByRole('heading', {
-        name: 'Installation',
-      });
+test('Verify that invalid moves (filled cells) are not accepted', async ({
+  page,
+}) => {
+  await page.goto('https://tic-tac-toe-tau-lyart-50.vercel.app/');
+  const cellOne = page.getByRole('button', { name: 'Cell 1' });
 
-      // Expects page to have a heading with the name of Installation.
-      await expect(installationHeading).toBeVisible();
-    },
-  );
+  // Click the 1st cell in the grid.
+  await cellOne.click();
 
-  test('visits torun website', async ({ page }) => {
-    await page.goto('https://www.google.com');
-    await page.getByRole('button', { name: 'Alle akzeptieren' }).click();
-    await page.getByRole('combobox', { name: 'Suche' }).fill('Torun Wikström');
-    await page
-      .getByRole('button', { name: 'Google Suche', exact: true })
-      .first()
-      .click();
-
-    await page.getByText('Torun Wikström');
-  });
+  // Expect cell 1 to be disabled
+  await expect(cellOne).toBeDisabled();
 });
